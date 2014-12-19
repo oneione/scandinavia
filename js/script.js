4e148be8fc7589af8f90baef596f7af76d20eea8
AndreyAA <avanal@mail.ru>
2014-12-19 12:50:33 +0300

    about page

--- js/script.js
+++ js/script.js
@@ -13,4 +13,28 @@
         }
     });
 
+
+    //about tabs:
+
+    var vacancyTab = new riverHouse.TabController({
+        tabLinkSelector: '.about__link',
+        tabLinkAttr: 'href',
+        tabWindowSelector: '.about__content',
+        openTabNumber: 0,
+        init: function($firstLink, $firstWindow, showTab) {
+            if(!$firstLink.length) return;
+            showTab($firstLink,$firstWindow);
+        },
+        beforeShow: function($activeTabLink, $activeTabWindow) {
+            //Здесь можно юзать аякс например:
+            console.log('Перед показом вкладки: ', $activeTabLink, $activeTabWindow);
+        },
+        afterShow: function(l,$activeTabWindow){
+            var tabWindowHeight = $activeTabWindow.outerHeight();
+            $('.page-content').css({
+                minHeight: tabWindowHeight
+            });
+        }
+    });
+
 })(jQuery);
